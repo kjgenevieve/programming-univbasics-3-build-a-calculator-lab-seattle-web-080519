d761683d9ba78edf57ebcbff3daded4dd28807eb
Genevieve <kj.genevieve@gmail.com>
2019-07-16 02:29:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,10 @@
 # addition - Build the method addition that adds num1 to num2 and returns the result of this calculation
-
 def addition(num1, num2)
   sum = num1 + num2
   p sum
 end
 
-
 # subtraction - Build the method subtraction that subtracts num1 from num2 and returns the result of this calculation
-
 def subtraction(num1, num2)
   difference = num1 - num2
   p difference
@@ -16,11 +13,17 @@ end
 
 
 
-# # division - Build the method division that divides num1 into num2 and returns the result of this calculation
+# division - Build the method division that divides num1 into num2 and returns the result of this calculation
 
-# def division(num1, num2)
-# quotient
-# end
+def division(num1, num2)
+  quotient = num1 / num2
+  p quotient
+end
+
+
+
+
+
 
 
 # # multiplication - Build the method multiplication that multiplies num1 from num2 and returns the result of this calculation
